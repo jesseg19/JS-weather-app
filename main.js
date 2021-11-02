5dae227ec55c0ed8f88305a79088298b756e6ffc
Jesse Gross <jessegross1919@gmail.com>
2021-11-01 22:37:09 -0400

    updated design

--- main.js
+++ main.js
@@ -32,14 +32,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
     //call this function
     .then(function (data) {
       //set all the current weather data
-      currentTemp.innerText = data.current.temp;
-      feelsLike.innerText = data.current.feels_like;
-      windSpeed.innerText = data.current.wind_speed;
+      currentTemp.innerText = Math.round(data.current.temp);
+      feelsLike.innerText = Math.round(data.current.feels_like);
+      windSpeed.innerText = Math.round(data.current.wind_speed);
       //calculate the north,east,south,west wind direction
       if (data.current.wind_deg >= 340 || data.current.wind_deg <= 20) {
         windDirection.innerText = "N";
       }
-      windDirection.innerText = data.current.wind_deg;
+      //windDirection.innerText = data.current.wind_deg;
       humidity.innerText = data.current.humidity;
       UVIndex.innerText = data.current.uvi;
       cloudiness.innerText = data.current.clouds;
@@ -55,7 +55,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
       hourlyPrev.addEventListener("click", hourlyBack);
       //call function to chnage the backaground based on current weather
       changeBackground();
-      currentUnits();
       updateHourlyWeather();
 
       function updateHourlyWeather() {
@@ -63,6 +62,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         for (let i = 0; i < hourlyCards.length; i++) {
           if (currentHour + hourOffset >= 20) {
             hourOffset = 0;
+
             if (AMPM === "PM") {
               AMPM = "AM";
             } else {
@@ -79,9 +79,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
               currentHour + i + hourOffset + AMPM;
           }
           hourlyCards[i].children[2].innerText =
-            data.hourly[i + hourOffset].temp + "°C";
+            Math.round(data.hourly[i + hourOffset].temp) + "°C";
           hourlyCards[i].children[3].innerText =
-            "Feels " + data.hourly[i + hourOffset].feels_like + "°C";
+            "Feels " +
+            Math.round(data.hourly[i + hourOffset].feels_like) +
+            "°C";
           hourlyCards[i].children[4].innerText =
             data.hourly[i + hourOffset].pop * 100 + "%";
         }
@@ -92,14 +94,47 @@ document.addEventListener("DOMContentLoaded", function (event) {
         let AMPM = "AM";
         hourOffset += 1;
         for (let i = 0; i < hourlyCards.length; i++) {
-          // console.log(hourOffset + currentHour);
           updateHourlyWeather();
         }
       }
 
       //go back by 5 hours and display the new data
       function hourlyBack() {
-        console.log(hourOffset);
+        let AMPM = "AM";
+        hourOffset -= 1;
+        console.log("current Hour: " + currentHour);
+        console.log("Hour offset: " + hourOffset);
+
+        for (let i = hourlyCards.length; i == 0; i--) {
+          //loop over the hourlyCards node list to set the hourly data intially
+          if (currentHour + hourOffset >= 20) {
+            hourOffset = 0;
+
+            if (AMPM === "PM") {
+              AMPM = "AM";
+            } else {
+              AMPM = "PM";
+            }
+            hourlyCards[i].children[1].innerText = 12 + AMPM;
+          } else if (currentHour + hourOffset > 12) {
+            AMPM = "PM";
+            hourlyCards[i].children[1].innerText =
+              currentHour + hourOffset - 12 + AMPM;
+            console.log("hey");
+          } else {
+            AMPM = "AM";
+            hourlyCards[i].children[1].innerText =
+              currentHour + hourOffset + AMPM;
+          }
+          hourlyCards[i].children[2].innerText =
+            Math.round(data.hourly[hourOffset].temp) + "°C";
+          hourlyCards[i].children[3].innerText =
+            "Feels " + Math.round(data.hourly[hourOffset].feels_like) + "°C";
+          hourlyCards[i].children[4].innerText =
+            data.hourly[hourOffset].pop * 100 + "%";
+        }
+
+        /*console.log(hourOffset);
         console.log(hourlyCards.length);
         if (hourOffset > 1) {
           hourOffset -= 1;
@@ -119,7 +154,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
             hourlyCards[i].children[4].innerText =
               data.hourly[i + hourOffset].pop * 100 + "%";
           }
-        }
+        }*/
       }
 
       //change the background depending on the time of day and cloudiness
@@ -139,43 +174,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
             "url('/img/cloudy.jpg') no-repeat 10% center";
           document.body.style.color = "white";
         }
-      }
-
-      function currentUnits() {
-        let metric = document.getElementById("C");
-        let imperial = document.getElementById("F");
-        metric.addEventListener("click", () => {
-          if (units === "imperial") {
-            units = "metric";
-            url =
-              "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-              lat +
-              "&lon=" +
-              lon +
-              "&units=" +
-              units +
-              "&appid=" +
-              apiKey;
-            call = fetch(url);
-          }
-        });
-
-        imperial.addEventListener("click", () => {
-          console.log("hey");
-          if (units === "metric") {
-            units = "imperial";
-            url =
-              "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-              lat +
-              "&lon=" +
-              lon +
-              "&units=" +
-              units +
-              "&appid=" +
-              apiKey;
-            call = fetch(url);
-          }
-        });
       }
     });
 });
